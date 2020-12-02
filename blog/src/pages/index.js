3aac1cb7da12066ab4fac71490a5632dca8d0591
Gilberto Vazquez <gilbertovazquez90@Gilbertos-MBP.attlocal.net>
2020-12-01 22:41:41 -0600

    init

---
+++ blog/src/pages/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Welcome!</h1>
+    <h2>My name is Gilberto and this is my blog! I'm also a web development student.</h2>
+    <p>Need a web developer? <Link to="/contact">Click Here</Link></p>
+  </Layout>
+)
+
+export default IndexPage
